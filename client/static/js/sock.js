da7bf3b3a1e125114c6fa2d1a644569c4dde8a5b
Todo Lodo <me@todolodo.xyz>
2024-02-04 01:02:58 +0530

    client auth update (might need more improvement)

--- client/static/js/sock.js
+++ client/static/js/sock.js
@@ -1,4 +1,8 @@
-const socket = io('http://localhost:5000/', {
+
+// Construct the full URL
+const sockUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
+
+const socket = io(sockUrl, {
     query: {
         token: localStorage.token  // Pass your authentication token here
     }
@@ -8,4 +12,11 @@ socket.on('auth', (data) => {
     console.log(data)
     console.log('Authentication Status:', data.status);
     localStorage.token = data.token
+});
+
+// Listen for the 'cookie_set' event to handle setting the cookie on the client side
+socket.on('cookie_set', (data) => {
+    const expiryDate = new Date(data.expiry_time * 1000); // Convert timestamp to milliseconds
+    document.cookie = `${data.cookie_name}=${data.cookie_value}; expires=${expiryDate.toUTCString()}`;
+    console.log(`Cookie set: ${document.cookie}`);
 });
--- server/Controllers.py
+++ server/Controllers.py
@@ -1,5 +1,7 @@
+import datetime
+import uuid
 from flask_socketio import SocketIO, join_room, emit
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from server.GameUtils import *
 
 
@@ -7,6 +9,7 @@ class SocketController:
     def __init__(self, socketio: SocketIO, players: Players):
         self.__io = socketio
         self.__players = players
+        self.__sessions = {}
 
     def control(self):
         @self.__io.on('connect')
@@ -18,23 +21,31 @@ class SocketController:
             _player = self.__players[_token]
 
             if _player is None:
-                _player = self.__players.newPlayer("custom-token")
+                uuid1 = uuid.uuid1()
+                uuid4 = str(uuid.uuid4()).replace("-", "")
+                uuid3 = str(uuid.uuid3(uuid1, client_address)).replace("-", "")
+                _player = self.__players.newPlayer(f"{uuid4}.{uuid3}")
                 _token = _player.token
 
             _player.connect()
 
-            print(_player)
+            print(_player, "player")
+
+            self.__sessions[request.sid] = _player
 
             join_room(sid=request.sid, room=_token)
 
             emit("auth", {"token": _token}, to=_token)
-
-            request.token = _token
+            emit('cookie_set',
+                 {'cookie_name': 'auth_token', 'cookie_value': _token, 'expiry_time': (datetime.datetime.now()+datetime.timedelta(hours=1)).timestamp()})
 
         @self.__io.on("disconnect")
         def disconnect_handler():
-            _player = self.__players[request.token]
+
+            _player = self.__sessions[request.sid]
             _player.disconnect()
+
+            self.__sessions.pop(request.sid, None)
 
 
 class HTTPController:
@@ -47,9 +58,10 @@ class HTTPController:
     def control(self):
         @self.__app.route("/controller", methods=["GET"])
         def controller():
-            print("hereeeee", self.__app.template_folder)
+            print(request.cookies.get('auth_token'))
             return render_template("controller.html")
 
         @self.__app.route("/test", methods=["GET"])
         def test():
+            print(request.cookies.get('auth_token'))
             return render_template("test2.html")
