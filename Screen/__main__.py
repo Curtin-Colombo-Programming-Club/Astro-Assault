0c8740155cf5c2d782f78c8ce4255414f41d2aa5
Todo Lodo <me@todolodo.xyz>
2024-02-19 20:59:13 +0530

    minor u

--- Screen/__main__.py
+++ Screen/__main__.py
@@ -11,8 +11,8 @@ def main():
     parser = argparse.ArgumentParser(description='Description of your program')
 
     # Add arguments
-    parser.add_argument('--host', type=int, help='Description of arg1')
-    parser.add_argument('--port', '-p', type=float, help='Description of arg2')
+    parser.add_argument('--host', type=str, help='Description of arg1')
+    parser.add_argument('--port', '-p', type=int, help='Description of arg2')
 
     # Parse the command-line arguments
     args = parser.parse_args()
