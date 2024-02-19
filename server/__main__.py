09ded6d36db7013a21ea244a705bf4130ea5f032
Todo Lodo <me@todolodo.xyz>
2024-02-19 21:15:30 +0530

    ef

--- server/__main__.py
+++ server/__main__.py
@@ -1,3 +1,8 @@
+import os, sys
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(CURRENT_DIR))
+sys.path.append(fr"{os.path.dirname(CURRENT_DIR)}\Screen")
+
 import Server
 import threading
 import argparse
